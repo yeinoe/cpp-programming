6df12df796f360046154e64f9986ab9038f93f01
yeinoe <117074430+yeinoe@users.noreply.github.com>
2023-10-05 22:56:47 +0900

    Create practice1-1.cpp

---
+++ 1week/practice1-1.cpp
@@ -0,0 +1,21 @@
+//예제 1-1 기본 c++ 프로그램
+
+#include
+using namesapce std;
+
+int g=20;
+int add(int x, int y) {
+  return x + y;
+}
+
+int main() {
+  int a, b, sum;
+cout >> a >> b;
+sum = a + b;
+cout << "합은 " << sum << "\n";
+cout << "합은 " << add(a, b) << "\n";
+cout << "전역 변수 g값은 " << d;
+
+return 0;
+}
+
