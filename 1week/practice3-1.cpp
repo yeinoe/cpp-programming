13a1d56fbb495b01d631eb0866273fcde85fc3ba
yeinoe <117074430+yeinoe@users.noreply.github.com>
2023-10-05 23:08:02 +0900

    Create practice3-1.cpp

---
+++ 1week/practice3-1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int score;
+	cout << "점수를 입력하세요>>";
+	cin >> score;
+	if (score > 100 || score < 0) {
+		cout << "잘못된 점수 입니다";
+		return 0;
+	}
+	if (score >= 90)
+		cout << "A 입니다";
+	else if (score >= 80)
+		cout << "B 입니다";
+	else if (score >= 70)
+		cout << "c 입니다";
+	else if (score >= 60)
+		cout << "D 입니다";
+	else
+		cout << "F 입니다";
+}
